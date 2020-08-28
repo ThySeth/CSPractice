d09a1acbe692d18670f15929b0fea67d5c853d0a
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-28 02:00:06 -0500

    This is required in ANY program/processes

---
+++ MANDATORY.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace theBeginning {
+  class program {
+    static void Main(string[] args) {
+      // Put your code here!
+      Console.WriteLine("Greetings, Earth!");
+      //
+    }
+  }
+}
