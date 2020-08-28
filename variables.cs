5ed7664bdcb0f77c93078b9cf2f597a5289dccd0
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-28 01:52:10 -0500

    Create variables.cs

---
+++ variables.cs
@@ -0,0 +1,19 @@
+int a = 10;
+
+Console.WriteLine(a);
+
+bool b = true;
+
+Console.WriteLine(b);
+
+string c = "Hello, World!";
+
+Console.WriteLine(c);
+
+char d = "D";
+
+Console.WriteLine(d);
+
+double e = 10.25;
+
+Console.WriteLine(e);
