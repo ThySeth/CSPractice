8861dcb0db66a595e47b787012debd3ad1de7b96
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-28 23:14:19 -0500

    Update IfElse.cs

--- IfElse.cs
+++ IfElse.cs
@@ -6,3 +6,11 @@ double b = Console.ReadLine();
 
 double sum = a + b;
 Console.WriteLine($"And the grand total is... {sum}!!!");
+
+if(a < b) {
+  Console.WriteLine($"{b} is greater than {a}!") 
+} else if(a > b) {
+  Console.WriteLine($"{a} is greater than {b}!"); 
+} else {
+  Console.WriteLine($"{a} and {b} are equal!"); 
+}
