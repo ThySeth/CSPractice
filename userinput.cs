767d297c86e135a44acc972013413682f8017bc0
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-28 22:33:02 -0500

    Create userinput.cs

---
+++ userinput.cs
@@ -0,0 +1,9 @@
+Console.WriteLine("What's ya favorite food?");
+
+string food = Console.ReadLine();
+
+Console.WriteLine("Huh, " + food + " eh? How many calories does it have?");
+
+double cal = Console.ReadLine();
+
+Console.WriteLine("Jeez " + cal + " calories?!");
