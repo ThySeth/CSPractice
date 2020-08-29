ad9e5522eb67746ebb8aa73b596bf63fcc394443
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-28 22:53:51 -0500

    Create strings.cs

---
+++ strings.cs
@@ -0,0 +1,16 @@
+// Concat //
+string part1 = "California";
+
+string part2 = "knows how to party!";
+
+string full = string.Concat(part1, part2);
+
+// Interpolations //
+
+string city = "San Jose";
+
+string state = "California";
+
+string place = $"Welcome to {city}, {state}!"; // Returns "Welcome to San Jose, California"
+
+//
