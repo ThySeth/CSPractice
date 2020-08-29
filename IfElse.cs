4f6189a36757d00126ce132a256dc6097467cfb3
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-28 23:09:24 -0500

    Create IfElse.cs

---
+++ IfElse.cs
@@ -0,0 +1,8 @@
+Console.WriteLine("Give me a number, any number!");
+double a = Console.ReadLine();
+
+Console.WriteLine($"Alright we got {a}, now give me another!");
+double b = Console.ReadLine();
+
+double sum = a + b;
+Console.WriteLine($"And the grand total is... {sum}!!!");
