daa4b6a7c55847cad8b68ef534f8f13eb491382c
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-28 22:42:09 -0500

    Create math.cs

---
+++ math.cs
@@ -0,0 +1,7 @@
+Math.Max(2, 21); // Returns highest number (21)
+
+Math.Min(12, 5); // Returns the lowest number (5)
+
+Math.Abs(-166); // Returns the absolute value of -166 (166)
+
+Math.Round(2.55); // Rounds to the nearest integer (3)
